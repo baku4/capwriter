138a9c02f0b65d9645b9a68a8d1210219f6ae540
baku4 <bahkhun@gmail.com>
2025-03-28 01:14:59 +0900

    Add tests & update to 0.3.1

--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -17,9 +17,8 @@ fn usage_on_readme() {
     assert_eq!(vec_to_save, vec_loaded);
 }
 
-
 #[test]
-fn are_equal_saved_and_loaded_for_all_vector() {
+fn are_equal_saved_and_loaded_for_vector() {
     use rand::Rng;
     let mut rng = rand::thread_rng();
 
@@ -28,199 +27,95 @@ fn are_equal_saved_and_loaded_for_all_vector() {
 
     for len in vec_len_list {
         for _ in 0..n {
-            // u8
-            {
-                let vec: Vec<u8> = (0..len).map(|_| { rng.gen() }).collect();
-                let mut buffer = Vec::new();
+            macro_rules! test {
+                ($ty:ty) => {
+                    let vec: Vec<$ty> = (0..len).map(|_| { rng.gen() }).collect();
+                    let mut buffer = Vec::new();
 
-                vec.save_to(&mut buffer).unwrap();
+                    vec.save_to(&mut buffer).unwrap();
 
-                let loaded = Vec::load_from(&mut &buffer[..]).unwrap();
+                    let loaded = Vec::<$ty>::load_from(&mut &buffer[..]).unwrap();
 
-                assert_eq!(vec, loaded);
+                    assert_eq!(vec, loaded);
+                };
             }
-            // u16
-            {
-                let vec: Vec<u16> = (0..len).map(|_| { rng.gen() }).collect();
-                let mut buffer = Vec::new();
-
-                vec.save_to(&mut buffer).unwrap();
-
-                let loaded = Vec::load_from(&mut &buffer[..]).unwrap();
-
-                assert_eq!(vec, loaded);
-            }
-            // u32
-            {
-                let vec: Vec<u32> = (0..len).map(|_| { rng.gen() }).collect();
-                let mut buffer = Vec::new();
-
-                vec.save_to(&mut buffer).unwrap();
-
-                let loaded = Vec::load_from(&mut &buffer[..]).unwrap();
-
-                assert_eq!(vec, loaded);
-            }
-            // u64
-            {
-                let vec: Vec<u64> = (0..len).map(|_| { rng.gen() }).collect();
-                let mut buffer = Vec::new();
-
-                vec.save_to(&mut buffer).unwrap();
-
-                let loaded = Vec::load_from(&mut &buffer[..]).unwrap();
-
-                assert_eq!(vec, loaded);
-            }
-            // u128
-            {
-                let vec: Vec<u128> = (0..len).map(|_| { rng.gen() }).collect();
-                let mut buffer = Vec::new();
-
-                vec.save_to(&mut buffer).unwrap();
-
-                let loaded = Vec::load_from(&mut &buffer[..]).unwrap();
-
-                assert_eq!(vec, loaded);
-            }
-            // usize
-            {
-                let vec: Vec<usize> = (0..len).map(|_| { rng.gen() }).collect();
-                let mut buffer = Vec::new();
-
-                vec.save_to(&mut buffer).unwrap();
-
-                let loaded = Vec::load_from(&mut &buffer[..]).unwrap();
-
-                assert_eq!(vec, loaded);
-            }
-
-            // i8
-            {
-                let vec: Vec<i8> = (0..len).map(|_| { rng.gen() }).collect();
-                let mut buffer = Vec::new();
-
-                vec.save_to(&mut buffer).unwrap();
-
-                let loaded = Vec::load_from(&mut &buffer[..]).unwrap();
-
-                assert_eq!(vec, loaded);
-            }
-            // i16
-            {
-                let vec: Vec<i16> = (0..len).map(|_| { rng.gen() }).collect();
-                let mut buffer = Vec::new();
-
-                vec.save_to(&mut buffer).unwrap();
-
-                let loaded = Vec::load_from(&mut &buffer[..]).unwrap();
-
-                assert_eq!(vec, loaded);
-            }
-            // i32
-            {
-                let vec: Vec<i32> = (0..len).map(|_| { rng.gen() }).collect();
-                let mut buffer = Vec::new();
-
-                vec.save_to(&mut buffer).unwrap();
-
-                let loaded = Vec::load_from(&mut &buffer[..]).unwrap();
-
-                assert_eq!(vec, loaded);
-            }
-            // i64
-            {
-                let vec: Vec<i64> = (0..len).map(|_| { rng.gen() }).collect();
-                let mut buffer = Vec::new();
-
-                vec.save_to(&mut buffer).unwrap();
-
-                let loaded = Vec::load_from(&mut &buffer[..]).unwrap();
-
-                assert_eq!(vec, loaded);
-            }
-            // i128
-            {
-                let vec: Vec<i128> = (0..len).map(|_| { rng.gen() }).collect();
-                let mut buffer = Vec::new();
-
-                vec.save_to(&mut buffer).unwrap();
-
-                let loaded = Vec::load_from(&mut &buffer[..]).unwrap();
-
-                assert_eq!(vec, loaded);
-            }
-            // isize
-            {
-                let vec: Vec<isize> = (0..len).map(|_| { rng.gen() }).collect();
-                let mut buffer = Vec::new();
-
-                vec.save_to(&mut buffer).unwrap();
-
-                let loaded = Vec::load_from(&mut &buffer[..]).unwrap();
-
-                assert_eq!(vec, loaded);
-            }
+            
+            test!(u8);
+            test!(u16);
+            test!(u32);
+            test!(u64);
+            test!(u128);
+            test!(usize);
+            test!(i8);
+            test!(i16);
+            test!(i32);
+            test!(i64);
+            test!(i128);
+            test!(isize);
         }
     }
 }
 
 #[test]
-fn is_accurate_to_be_saved_size() {
+fn is_accurate_to_be_saved_size_for_vector() {
     for n in 1..30 {
-        {
-            let mut buffer: Vec<u8> = Vec::new();
-            let u8_vec = vec![0_u8; n];
-            u8_vec.save_to(&mut buffer).unwrap();
-            assert_eq!(u8_vec.to_be_saved_size(), buffer.len());
+        macro_rules! test {
+            ($ty:ty) => {
+                let mut buffer: Vec<u8> = Vec::new();
+                let vec_to_save: Vec<$ty> = vec![0; n];
+                vec_to_save.save_to(&mut buffer).unwrap();
+                assert_eq!(vec_to_save.to_be_saved_size(), buffer.len());
+            };
         }
-        {
-            let mut buffer: Vec<u8> = Vec::new();
-            let u16_vec = vec![0_u16; n];
-            u16_vec.save_to(&mut buffer).unwrap();
-            assert_eq!(u16_vec.to_be_saved_size(), buffer.len());
+
+        test!(u8);
+        test!(u16);
+        test!(u32);
+        test!(u64);
+        test!(u128);
+        test!(usize);
+        test!(i8);
+        test!(i16);
+        test!(i32);
+        test!(i64);
+        test!(i128);
+        test!(isize);
+    }
+}
+
+#[test]
+fn are_equal_saved_and_loaded_for_array() {
+    use rand::Rng;
+    let mut rng = rand::thread_rng();
+    let n = 100;
+
+    for _ in 0..n {
+        macro_rules! test {
+            ($ty:ty, [ $($size:literal),+ ]) => {
+                $(
+                    {
+                        // 여기서 $size는 컴파일 타임 상수
+                        let array: [$ty; $size] = [rng.gen(); $size];
+                        let mut buffer = Vec::new();
+                        array.save_to(&mut buffer).unwrap();
+                        let loaded = <[$ty; $size]>::load_from(&mut &buffer[..]).unwrap();
+                        assert_eq!(array, loaded, "Failed at size {}", $size);
+                    }
+                )*
+            }
         }
-        {
-            let mut buffer: Vec<u8> = Vec::new();
-            let u32_vec = vec![0_u32; n];
-            u32_vec.save_to(&mut buffer).unwrap();
-            assert_eq!(u32_vec.to_be_saved_size(), buffer.len());
-        }
-        {
-            let mut buffer: Vec<u8> = Vec::new();
-            let u64_vec = vec![0_u64; n];
-            u64_vec.save_to(&mut buffer).unwrap();
-            assert_eq!(u64_vec.to_be_saved_size(), buffer.len());
-        }
-        {
-            let mut buffer: Vec<u8> = Vec::new();
-            let usize_vec = vec![0_usize; n];
-            usize_vec.save_to(&mut buffer).unwrap();
-            assert_eq!(usize_vec.to_be_saved_size(), buffer.len());
-        }
-        {
-            let mut buffer: Vec<u8> = Vec::new();
-            let i16_vec = vec![0_i16; n];
-            i16_vec.save_to(&mut buffer).unwrap();
-            assert_eq!(i16_vec.to_be_saved_size(), buffer.len());
-        }
-        {
-            let mut buffer: Vec<u8> = Vec::new();
-            let i32_vec = vec![0_i32; n];
-            i32_vec.save_to(&mut buffer).unwrap();
-            assert_eq!(i32_vec.to_be_saved_size(), buffer.len());
-        }
-        {
-            let mut buffer: Vec<u8> = Vec::new();
-            let i64_vec = vec![0_i64; n];
-            i64_vec.save_to(&mut buffer).unwrap();
-            assert_eq!(i64_vec.to_be_saved_size(), buffer.len());
-        }
-        {
-            let mut buffer: Vec<u8> = Vec::new();
-            let isize_vec = vec![0_isize; n];
-            isize_vec.save_to(&mut buffer).unwrap();
-            assert_eq!(isize_vec.to_be_saved_size(), buffer.len());
-        }
+            
+        test!(u8, [1, 2, 4, 8, 16, 32, 64]);
+        test!(u16, [1, 2, 4, 8, 16, 32, 64]);
+        test!(u32, [1, 2, 4, 8, 16, 32, 64]);
+        test!(u64, [1, 2, 4, 8, 16, 32, 64]);
+        test!(u128, [1, 2, 4, 8, 16, 32, 64]);
+        test!(usize, [1, 2, 4, 8, 16, 32, 64]);
+        test!(i8, [1, 2, 4, 8, 16, 32, 64]);
+        test!(i16, [1, 2, 4, 8, 16, 32, 64]);
+        test!(i32, [1, 2, 4, 8, 16, 32, 64]);
+        test!(i64, [1, 2, 4, 8, 16, 32, 64]);
+        test!(i128, [1, 2, 4, 8, 16, 32, 64]);
+        test!(isize, [1, 2, 4, 8, 16, 32, 64]);
     }
 }
